15188e6d685eb337470064c2382a1bb20ced2fe8
Rafael Vargas <rafael.vargas@outlook.com>
2022-05-04 20:07:31 -0400

    install react bootstrap and add bootstrap navbar and links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import "./App.css";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
+import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to='/'>Caroline Dambrosio Designs</Link>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>Caroline Dambrosio Designs</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
