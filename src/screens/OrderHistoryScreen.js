f27b1cd8371087fa3e71b537b587a7f0804ca44c
cantstopcoding <jonathan_koss@yahoo.com>
2022-05-29 11:48:31 -0400

    fix typos

--- src/screens/OrderHistoryScreen.js
+++ src/screens/OrderHistoryScreen.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
-import { Button } from 'bootstrap';
 import React, { useContext, useEffect, useReducer } from 'react';
 import { Helmet } from 'react-helmet-async';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { Store } from '../Store';
 import { getError } from '../utils';
+import { Button } from 'react-bootstrap';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -30,30 +30,33 @@ export default function OrderHistoryScreen() {
     loading: true,
     error: '',
   });
-
   useEffect(() => {
     const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const { data } = await axios.get(`/api/orders/mine`, {
-          headers: { Authorization: `Bearer ${userInfo.token}` },
-        });
+        const { data } = await axios.get(
+          `/api/orders/mine`,
+
+          { headers: { Authorization: `Bearer ${userInfo.token}` } }
+        );
         dispatch({ type: 'FETCH_SUCCESS', payload: data });
       } catch (error) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(error) });
+        dispatch({
+          type: 'FETCH_FAIL',
+          payload: getError(error),
+        });
       }
     };
-
     fetchData();
   }, [userInfo]);
-
   return (
     <div>
       <Helmet>
         <title>Order History</title>
       </Helmet>
+
       <h1>Order History</h1>
-      {LoadingBox ? (
+      {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
         <MessageBox variant='danger'>{error}</MessageBox>
@@ -73,19 +76,21 @@ export default function OrderHistoryScreen() {
             {orders.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
-                <td>{order.createAt.substring(0, 10)}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
                 <td>{order.totalPrice.toFixed(2)}</td>
-                <td>{order.isPaid ? order.paidAt.subsctring(0, 10) : 'No'}</td>
+                <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
                 <td>
                   {order.isDelivered
-                    ? order.delvieredAt.substring(0, 10)
+                    ? order.deliveredAt.substring(0, 10)
                     : 'No'}
                 </td>
                 <td>
                   <Button
                     type='button'
                     variant='light'
-                    onClick={() => navigate(`/order/${order._id}`)}
+                    onClick={() => {
+                      navigate(`/order/${order._id}`);
+                    }}
                   >
                     Details
                   </Button>
