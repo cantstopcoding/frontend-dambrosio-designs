03c592a3908f1d7a06760474adc66928957cf042
cantstopcoding <jonathan_koss@yahoo.com>
2022-05-31 21:26:06 -0400

    finish profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,7 +1,25 @@
-import React, { useContext, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import React, { useContext, useReducer, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import { Store } from '../Store';
+import { toast } from 'react-toastify';
+import { getError } from '../utils';
+import axios from 'axios';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_REQUEST':
+      return { ...state, loadingUpdate: true };
+    case 'UPDATE_SUCCESS':
+      return { ...state, loadingUpdate: false };
+    case 'UPDATE_FAIL':
+      return { ...state, loadingUpdate: false };
+
+    default:
+      return state;
+  }
+};
 
 export default function ProfileScreen() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -11,13 +29,44 @@ export default function ProfileScreen() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
-  const submitHandler = async (e) => {};
+  const [{ loadingUpdate }, dispatch] = useReducer(reducer, {
+    loadingUpdate: false,
+  });
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        '/api/users/profile',
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({
+        type: 'UPDATE_SUCCESS',
+      });
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      toast.success('User updated successfully');
+    } catch (err) {
+      dispatch({
+        type: 'FETCH_FAIL',
+      });
+      toast.error(getError(err));
+    }
+  };
 
   return (
     <div className='container small-container'>
       <Helmet>
         <title>User Profile</title>
       </Helmet>
+      <h1 className='my-3'>User Profile</h1>
       <form onSubmit={submitHandler}>
         <Form.Group className='mb-3' controlId='name'>
           <Form.Label>Name</Form.Label>
@@ -51,7 +100,7 @@ export default function ProfileScreen() {
           />
         </Form.Group>
         <div className='mb-3'>
-          <Button type='submit'></Button>
+          <Button type='submit'>Update</Button>
         </div>
       </form>
     </div>
